98a93fdba1563e49381f60de5ee6ae2ee5edf660
rime.cyou <mail@rime.cyou>
2026-09-03 11:12:15 +0300

    refactor(web): keep split entry out of legacy sidebar

--- apps/web/src/components/LegacySidebar.tsx
+++ apps/web/src/components/LegacySidebar.tsx
@@ -93,7 +93,6 @@ import { selectThreadTerminalUiState, useTerminalUiStateStore } from "../termina
 import { useThreadRunningTerminalIds } from "../state/terminalSessions";
 import { useThreadDiscoveredPorts } from "../portDiscoveryState";
 import { openDiscoveredPort } from "./preview/openDiscoveredPort";
-import { openChatThreadInSplit } from "../chatWorkspaceStore";
 import { useAtomCommand } from "../state/use-atom-command";
 import { previewEnvironment } from "../state/preview";
 import {
@@ -2241,7 +2240,6 @@ const SidebarProjectItem = memo(function SidebarProjectItem(props: SidebarProjec
         thread.worktreePath ?? threadProject?.workspaceRoot ?? project.workspaceRoot ?? null;
       const clicked = await api.contextMenu.show(
         [
-          { id: "open-in-split", label: "Open in split" },
           ...(thread.branch
             ? [{ id: "new-thread-on-branch", label: `New thread on ${thread.branch}` }]
             : []),
@@ -2261,11 +2259,6 @@ const SidebarProjectItem = memo(function SidebarProjectItem(props: SidebarProjec
           to: "/projects/$projectKey",
           params: { projectKey: project.projectKey },
         });
-        return;
-      }
-      if (clicked === "open-in-split") {
-        openChatThreadInSplit(threadRef);
-        navigateToThread(threadRef);
         return;
       }
 
